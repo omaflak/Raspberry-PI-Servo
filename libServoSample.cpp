1e15c25eb3e51219e6fa3c3c0085c14624afc173
omaflak <omaflak@yahoo.fr>
2015-11-18 21:34:06 +0100

    some comments

--- libServoSample.cpp
+++ libServoSample.cpp
@@ -1,4 +1,4 @@
-#include <wiringPi.h>
+#include <wiringPi.h> // don't forget
 #include <cstdlib>
 #include "libServo.h"
 
@@ -13,8 +13,8 @@ int main()
         exit(1);
 
     Servo servo(pin);
-    servo.init(90);
+    servo.init(90); // initialize the servo to an angle. Need to be called only once
 
-    servo.move(180);
+    servo.move(180); // Then move the servo to any angle
     servo.move(0);
 }
